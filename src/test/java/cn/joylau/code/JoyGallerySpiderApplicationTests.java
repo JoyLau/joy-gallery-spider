1fcb0d59718f1fc4e0b22df890ab952ad7c1d02b
JoyLau <2587038142.liu@gmail.com>
2020-05-01 00:29:48 +0800

    init

---
+++ src/test/java/cn/joylau/code/JoyGallerySpiderApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.joylau.code;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoyGallerySpiderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
